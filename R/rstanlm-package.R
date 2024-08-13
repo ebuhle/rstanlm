eab83933ec392840ccdb825ddba1ffb7ad8f98d5
Eric Buhle <eric.buhle@noaa.gov>
2024-08-13 11:04:44 -0700

    Initial commit

---
+++ R/rstanlm-package.R
@@ -0,0 +1,15 @@
+#' The 'rstanlm' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @name rstanlm-package
+#' @aliases rstanlm
+#' @useDynLib rstanlm, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.3. https://mc-stan.org
+#'
+"_PACKAGE"
